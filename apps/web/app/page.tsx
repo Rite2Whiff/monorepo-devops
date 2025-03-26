02c0fe2efd3c07e03f572070415398fdee4298a3
Rite2Whiff <117234003+Rite2Whiff@users.noreply.github.com>
2025-03-26 12:07:39 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,9 @@ export default async function Home() {
   const user = await prismaClient.user.findFirst();
   return (
     <div>
+      Username:
       {user?.username}
+      Password:
       {user?.password}
     </div>
   );
